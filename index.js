84dc5f71fbda939388f454663aee73f88add3872
hassanalee <hassanali14563@gmail.com>
2024-05-25 14:52:07 +0500

    fixed a cors issue

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3001;
-const  Controller = require('./src/routes/routes');
-
-app.use('/customer',  Controller);
+const cors = require("cors");
+const port = 3000;
+const Controller = require("./src/routes/routes");
+app.use(cors());
+app.use("/customer", Controller);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
 app.use((req, res, next) => {
-  res.status(404).send('No Route Found');
+  res.status(404).send("No Route Found");
 });
